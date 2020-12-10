fd77cf5f4883ddaceec4097bc2cb62543f84ecc5
dmomoholle891 <73800204+dmomoholle891@users.noreply.github.com>
2020-12-10 11:57:20 +0100

    My Assignment
    Please check for correction

---
+++ hellopy.py
@@ -0,0 +1,40 @@
+print("Hello World!")
+print("Hello World, This is my first python class!")
+message = "This is the message I want you to see"
+print(message)
+
+# Write a program that calculate the area of a square
+# Write the formula: Length * Length
+# Measure the length using a device
+###################################
+# Convert your program to accept user input for the length variables and
+# use the input to compute the area of the square
+###################################
+# Ask the user for the length
+
+# Assigment 1
+# " Please enter the length of the square: "
+length = "length of square"
+# We are going to hardcode values; No dynamic values
+length = 6
+# Calculate the area of the square using the formular(Length * Length)
+# Read up: Variable naming styles: length_of_square  lengthOfSquare
+area = length * length
+print(area)
+
+# pysimplegui
+
+# Assignment 2
+name = "Deborah,"
+address = "N.C.C.E. Management Quuarters, Ibrahim Isiyaku Street, Mabushi, Abuja, Nigeria"
+print(name, address)
+
+# Assignment 3
+username = "Deborah"
+for x in range(10):
+    print(username)
+
+
+
+
+
